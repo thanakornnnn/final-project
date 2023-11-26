eca7b4622b0b36b1af007f17fffba15ce560f812
Thanakorn JAEMPRASERTKUL <thanakorn.jae@ku.th>
2023-11-26 21:00:57 +0700

    finish def login

--- project_manage.py
+++ project_manage.py
@@ -3,6 +3,15 @@ from database import *
 
 
 # define a funcion called initializing
+# here are things to do in this function:
+
+    # create an object to read all csv files that will serve as a persistent state for this program
+
+    # create all the corresponding tables for those csv files
+
+    # see the guide how many tables are needed
+
+    # add all these tables to the database
 database = DB()
 def initializing():
     read_person = Readcsv('persons.csv')
@@ -15,25 +24,19 @@ def initializing():
     user_table = Table('login', read_login.data)
     database.insert(user_table)
 
-# here are things to do in this function:
-
-    # create an object to read all csv files that will serve as a persistent state for this program
-
-    # create all the corresponding tables for those csv files
-
-    # see the guide how many tables are needed
-
-    # add all these tables to the database
-
-
 # define a funcion called login
-
-
 # here are things to do in this function:
    # add code that performs a login task
         # ask a user for a username and password
         # returns [ID, role] if valid, otherwise returning None
-
+def login():
+    ask_username = input('Enter username: ')
+    ask_password = input('Enter password: ')
+    search_login = database.search('login')
+    for i in search_login.data:
+        if ask_username == i['username'] and ask_password == i['password']:
+            return [i['ID'], i['role']]
+    return None
 # define a function called exit
 def exit():
     pass
