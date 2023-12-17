b286057b7255b3c2f45ec8d6b6f0a31d5e7f39b8
Thanakorn JAEMPRASERTKUL <thanakorn.jae@ku.th>
2023-12-17 07:11:16 +0700

    create csv

--- project_manage.py
+++ project_manage.py
@@ -1,80 +1,91 @@
 from database import *
 
-
-# define a funcion called initializing
-# here are things to do in this function:
-
-    # create an object to read all csv files that will serve as a persistent state for this program
-
-    # create all the corresponding tables for those csv files
-
-    # see the guide how many tables are needed
-
-    # add all these tables to the database
 database_n = DB()
+
+
 def initializing():
-    read_person = Readcsv('persons.csv')
-    read_person.read_csv()
-    persons_table = Table('persons', read_person.data)
-    database_n.insert(persons_table)
+    read_person = ReadCSV('persons.csv')
+    person_table = Table('Person table', read_person.data)
+    database_n.insert(person_table)
 
-    read_login = Readcsv('login.csv')
-    read_login.read_csv()
-    user_table = Table('login', read_login.data)
-    database_n.insert(user_table)
+    read_login = ReadCSV('login.csv')
+    login_table = Table('Login table', read_login.data)
+    database_n.insert(login_table)
 
-# define a funcion called login
-# here are things to do in this function:
-   # add code that performs a login task
-        # ask a user for a username and password
-        # returns [ID, role] if valid, otherwise returning None
+    project_data = [
+        {'ProjectID': None, 'Title': None, 'Lead': None, 'Member1': None,
+         'Member2': None, 'Advisor': None, 'Status': None}]
+    project_table = Table('Project table', project_data)
+    database_n.insert(project_table)
+
+    adPendReq_data = [
+        {'ProjectID': None, 'to_be_advisor': None, 'Response': None, 'Response_date': None}]
+    adPendReq_table = Table('Advisor_pending_request table', adPendReq_data)
+    database_n.insert(adPendReq_table)
+
+    memPendReq_data = [{'ProjectID': None, 'to_be_member': None, 'Response': None, 'Response_date': None}]
+    memPendReq_table = Table('Member_pending_request table', memPendReq_data)
+    database_n.insert(memPendReq_table)
+
 def login():
+    while True:
+        username = input("Enter username: ")
+        login_table = database_n.search('Login table')
+        user_found = next((user for user in login_table.data if user['username'] == username), None)
+        if user_found:
+            break
+        else:
+            print("Username not found. Please try again.")
+    password = input("Enter password: ")
+    if user_found['password'] == password:
+        return [user_found['ID'], user_found['role']]
+    else:
+        return None
 
-    ask_username = input('Enter username: ')
-    ask_password = input('Enter password: ')
-    search_login = database_n.search('login')
-    for i in search_login.data:
-        if ask_username == i['username'] and ask_password == i['password']:
-            return [i['ID'], i['role']]
-    return None
 
-# define a function called exit
 def exit():
+    """
+    here are things to do in this function:
+    - write out all the tables that have been modified to the corresponding csv files
+    - By now, you know how to read in a csv file and transform it into a list of dictionaries.
+        For this project, you also need to know how to do the reverse,
+        i.e., writing out to a csv file given a list of dictionaries.
+        See the link below for a tutorial on how to do this:
+        https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
+    """
     for table in database_n.database:
-        table_name = table.table_name
-        file_path = os.path.join(table.__location__, f'{table_name}.csv')
-        with open(file_path, 'w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=table.data[0].keys())
-            writer.writeheader()
-            writer.writerows(table.data)
-
-# here are things to do in this function:
-   # write out all the tables that have been modified to the corresponding csv files
-   # By now, you know how to read in a csv file and transform it into a list of dictionaries. For this project, you also need to know how to do the reverse, i.e., writing out to a csv file given a list of dictionaries. See the link below for a tutorial on how to do this:
-
-   # https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
+        filename = f"{table.table_name}.csv"
+        with open(filename, mode='w', newline='', encoding='utf-8') as file:
+            if table.data:
+                headers = table.data[0].keys()
+                writer = csv.DictWriter(file, fieldnames=headers)
+                writer.writeheader()
+                writer.writerows(table.data)
+            else:
+                print(f"Table '{table.table_name}' is empty. No CSV file created.")
+    print("All tables have been written out to CSV files.")
 
 
-# make calls to the initializing and login functions defined above
 
 initializing()
 val = login()
-print(val)
+
 
 # based on the return value for login, activate the code that performs activities according to the role defined for that person_id
 
 # if val[1] = 'admin':
-    # see and do admin related activities
+#     see and do admin related activities
 # elif val[1] = 'student':
-    # see and do student related activities
+#     see and do student related activities
 # elif val[1] = 'member':
-    # see and do member related activities
+#     see and do member related activities
 # elif val[1] = 'lead':
-    # see and do lead related activities
+#     see and do lead related activities
 # elif val[1] = 'faculty':
-    # see and do faculty related activities
+#     see and do faculty related activities
 # elif val[1] = 'advisor':
-    # see and do advisor related activities
+#     see and do advisor related activities
 
 # once everyhthing is done, make a call to the exit function
 exit()
+
